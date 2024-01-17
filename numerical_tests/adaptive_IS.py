fix numerical tests files

numerical_tests/adaptive_IS.py
@@ -69,18 +69,18 @@ def single_test(dim):
         init_mean = ot.Point(np.zeros(dim))
         init_cov_matrix = ot.CovarianceMatrix(1*np.eye(dim)) 
         init_distr = ot.Normal(init_mean,init_cov_matrix)
-        samples,vae = adaptive_is_vae(target_distr, init_distr, 10**4, 10,latent_dim=4)
+        samples,W,vae = adaptive_is_vae(target_distr, init_distr, 10**4, 5,latent_dim=4)
 
 
         xx = np.linspace(-6,6,1001).reshape((-1,1))
         yy = np.array(target_distr.getMarginal(0).computePDF(xx)).flatten()
         yy2 = np.array(init_distr.getMarginal(0).computePDF(xx)).flatten()
         
-        last_sample = np.array(samples[-1])
+        last_sample = np.array(samples)
         fig,ax = plt.subplots(2,5,figsize=(12,6))
         for i in range(2):
             for j in range(5):
-                ax[i,j].hist(last_sample[:,5*i+j],bins=100,density=True)
+                ax[i,j].hist(last_sample[:,5*i+j],bins=100,density=True,weights=W)
                 ax[i,j].plot(xx,yy)
                 ax[i,j].plot(xx,yy2)
                 
@@ -111,9 +111,12 @@ def single_test(dim):
                 ax[i,j].plot(xx,yy)
                 ax[i,j].plot(xx,yy2)
 
+    dkl = compute_Dkl(target_distr,vae[0],vae[1],vae[2])
+    print(dkl)
+
     return fig,ax
 
-fig,ax = single_test(20)    
+fig,ax = single_test(10)    
 
 #%% Test case 1
 

numerical_tests/four_branches.py
@@ -33,10 +33,10 @@ input_dim = 2
 latent_dim = 2
 N = 10**4
 
-x1_min = -5
-x1_max = 5
-x2_min = -5
-x2_max = 5
+x1_min = -6
+x1_max = 6
+x2_min = -6
+x2_max = 6
 
 n_points = 10**2+1
 
@@ -54,11 +54,11 @@ for i in tqdm(range(n_points)):
 ot_function = ot.PythonFunction(input_dim,1,four_branches)
 X = ot.Normal(input_dim).getSample(N)
 
-fig,ax = plt.subplots(figsize=(11,9))
-cnt = ax.contourf(X1, X2, values_function,levels=100)
+fig,ax = plt.subplots(figsize=(20,10))
+cnt = ax.contourf(X1, X2, values_function,levels=25,cmap="binary")
 ct = ax.contour(X1, X2, values_function, [t],colors='red',linewidths=3)
 
-cbar = fig.colorbar(cnt, ax=ax, ticks=[0,1,2,3,4,5,6,7])
+cbar = fig.colorbar(cnt, ax=ax, ticks=[0,1,2,3,4,5,6,7,8])
 for ta in cbar.ax.get_yticklabels():
      ta.set_fontsize(20)
 
@@ -68,7 +68,10 @@ ax.set_ylabel("$X_2$",fontsize=20)
 ax.tick_params(axis='x',labelsize='xx-large')
 ax.tick_params(axis='y',labelsize='xx-large')
 
-fig.savefig("Figures/four_branches_2d.png",bbox_inches='tight',dpi=500)
+ax.set_xlim(-6,6)
+ax.set_ylim(-6,6)
+
+fig.savefig("Figures/four_branches_2d_slide.png",bbox_inches='tight',dpi=500)
 
 #%% CE-VAE
 
@@ -81,12 +84,28 @@ proba, samples, N_tot = CEIS_VAE(N,.25, ot_function, t, input_distr,latent_dim=2
 print(f"Estimated failure probability is {proba} with {N_tot} calls to phi.")
 
 if input_dim==2:
-    fig, ax = plt.subplots(figsize=(15,15))
-    ax.contour(X1, X2, values_function, [t], colors="r", linewidths=3)
+    fig, ax = plt.subplots(figsize=(20,10))
+    cnt = ax.contourf(X1, X2, values_function,levels=25,cmap="binary")
+    
+    cbar = fig.colorbar(cnt, ax=ax, ticks=[0,1,2,3,4,5,6,7,8])
+    for ta in cbar.ax.get_yticklabels():
+         ta.set_fontsize(20)
+    
     for sample in samples:
-        ax.plot(*np.array(samples).T, ".", markersize=10)
-
-    fig.savefig("Figures/four_branches_ce_samples_dim2.png",bbox_inches='tight')
+        ax.plot(*np.array(samples).T, ".", markersize=4)
+    
+    ct = ax.contour(X1, X2, values_function, [t], colors="r", linewidths=3)
+    
+    plt.clabel(ct, inline=1, fontsize=30)
+    
+    
+    ax.set_xlim(-6,6)
+    ax.set_ylim(-6,6)
+    ax.set_title("Cross-entropy algorithm with a VAE as auxiliary density",fontsize=20)
+    ax.set_xlabel("$X_1$",fontsize=20)
+    ax.set_ylabel("$X_2$",fontsize=20)
+    ax.tick_params(axis='x',labelsize='xx-large')
+    ax.tick_params(axis='y',labelsize='xx-large')
 
 
 #%% Multiple runs
@@ -117,11 +136,10 @@ for n in tqdm(range(n_rep)):
     proba_vM,_,n_tot,_,_ = CEIS_vMFNM(N_ce,p,ot_function,t,input_distr,3)
     proba_ceis_vM_3[n] = proba_vM
     N_tots[n,2] = n_tot
-    
+        
     proba_vM,_,n_tot,_,_ = CEIS_vMFNM(N_ce,p,ot_function,t,input_distr,5)
     proba_ceis_vM_5[n] = proba_vM
     N_tots[n,3] = n_tot
-    
     
 
 #%% Save data
@@ -129,6 +147,6 @@ for n in tqdm(range(n_rep)):
 np.savez(f"Data/four_branches_failprob_estimations_{input_dim}.npz",
          CE_vae=proba_ceis_vae,
          CE_vM=proba_ceis_vM,
-         CE_vae_3=proba_ceis_vM_3,
-         CE_vae_5=proba_ceis_vM_5,
+         CE_vM_3=proba_ceis_vM_3,
+         CE_vM_5=proba_ceis_vM_5,
          N_tots=N_tots)